454a68781e575fa3e413fc415fcbd66be809c060
tylerjwashburn <tylerjwashburn@gmail.com>
2025-10-02 23:37:07 -0400

    10/2/25

--- ILE main.R
+++ ILE main.R
@@ -1,6 +1,7 @@
 library(dplyr)
 library(readxl)
 library(readr)
+library(EnvStats)
 ## Import Data Sets ##
 Creatinine <- read_csv("All creatinine 5-7-2025.csv")
 Mapping <- read_excel("2022-01432_SID-PID_mapping_202410091047_Combined.xlsx")
@@ -30,6 +31,35 @@ Neonic_full <- Neonic_Data %>%
     Creatinine_full %>% select(PID, TRIMESTER, creatinine),
     by = c("PID", "TRIMESTER")
   )
+## Overall Summary Stats ##
+summary_table <- Neonic_full %>%
+  group_by(Analyte_Code) %>%
+  summarise(
+    LOD = first(LOD),                                         # LOD
+    n = sum(!is.na(Concentration)),                           # Sample Size
+    geo_mean = round(geoMean(Concentration + 0.0001, na.rm = TRUE),2), # GM
+    p50 = round(quantile(Concentration, 0.50, na.rm = TRUE),2),        # 50th %
+    p75 = round(quantile(Concentration, 0.75, na.rm = TRUE),2),        # 75th %
+    p90 = round(quantile(Concentration, 0.90, na.rm = TRUE),2),        # 90th %
+    p95 = round(quantile(Concentration, 0.95, na.rm = TRUE),2),        # 95th %
+    IQR = round(IQR(Concentration, na.rm = TRUE),2)                    # IQR
+  )
+## Trimester Data ##
+Tri_table <- Neonic_full %>%
+  group_by(TRIMESTER, Analyte_Code) %>%
+  summarise(
+    LOD = first(LOD),                                         # LOD
+    n = sum(!is.na(Concentration)),                           # Sample Size
+    geo_mean = round(geoMean(Concentration + 0.0001, na.rm = TRUE),2), # GM
+    p50 = round(quantile(Concentration, 0.50, na.rm = TRUE),2),        # 50th %
+    p75 = round(quantile(Concentration, 0.75, na.rm = TRUE),2),        # 75th %
+    p90 = round(quantile(Concentration, 0.90, na.rm = TRUE),2),        # 90th %
+    p95 = round(quantile(Concentration, 0.95, na.rm = TRUE),2),        # 95th %
+    IQR = round(IQR(Concentration, na.rm = TRUE),2),                   # IQR
+    .groups = 'drop') 
 
+summary_T1 <- Tri_table %>% filter(Tri_table$TRIMESTER == 1)
+summary_T2 <- Tri_table %>% filter(Tri_table$TRIMESTER == 2)
+summary_T3 <-Tri_table %>% filter(Tri_table$TRIMESTER == 3)
 
-
+## FOD is an outlier thing? ##
